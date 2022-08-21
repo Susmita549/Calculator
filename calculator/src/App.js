86d0aee7eb89d6d927e3f0e1f16a750b2713481e
Susmita Mandal <99960489+Susmita549@users.noreply.github.com>
2022-08-21 08:36:26 +0530

    calculator created

---
+++ calculator/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Calculator from './components/Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container border border-primary'>
+        <Calculator/>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
